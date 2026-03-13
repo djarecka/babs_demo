5cc0efef7049e1ef26b66d40677ce381fa4284a7
Dorota Jarecka <djarecka@gmail.com>
2026-03-13 14:43:50 -0400

    update

---
+++ babs_walkthrough_merge.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash                                                                                          
+
+set -eux
+PS4='> '
+
+# Accept dataset name and input path as arguments
+BABS_PROJECT="$1"  # Accept site name
+BASENAME=$(basename "$BABS_PROJECT")
+OUTPUT_DIR="$2"  # Accept dataset name as second argument
+TARGET_DIR="${OUTPUT_DIR}/${BASENAME}"
+
+
+echo ""
+echo "=== Step 4: Merge results of the project:" ${BASENAME}
+echo "target dir" ${TARGET_DIR}
+exit 0
+
+cd "${BABS_PROJECT}"
+babs merge
+
+echo "Cloning output RIA store..."
+datalad clone \
+    "ria+file://${BABS_PROJECT}/output_ria#~data" \
+    "$TARGET_DIR"
+
+echo "Listing outputs:"
+cd "$TARGET_DIR"
+ls
+
+echo ""
+echo "=== Walkthrough complete ==="
+echo "To inspect a specific subject's output, run:"
+echo "  datalad get <subject_zip_file>"
+echo "  unzip -l <subject_zip_file>"
